88d933718e402b4a212d54b580e85d3c3a6ee707
sol - Andrii Vasyliev <andrii.vasyliev@gmail.com>
2014-11-27 00:27:44 +0200

    reINITed sol home repository

---
+++ fun.some.pp
@@ -0,0 +1,24 @@
+// best
+#include	"best.some"
+
+// unsorted
+#include	"jokes.some"
+
+// начинаются со слов
+#include	"esli.some"
+#include	"chem.some"
+
+// добрые, злые, умные
+#include	"kind.some"
+#include	"evel.some"
+#include	"smart.some"
+
+// тематические
+#include	"KinDzaDza.some"
+#include	"mat.some"
+#include	"pol.some"
+#include	"programmers.some"
+#include	"vodka.some"
+#include	"woman.some"
+#include	"slova.some"
+
